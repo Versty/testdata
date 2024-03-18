1ae3e89a787519651d4e32fbbd817bc8c078b656
Diana <107266340+Versty@users.noreply.github.com>
2024-03-18 17:05:10 +0300

    Update empty_script.js

--- empty_script.js
+++ empty_script.js
@@ -1,6 +1,6 @@
 // ==UserScript==
-// @name         script doing nothing
-// @version      0.5
+// @name         scripttt doing nothing
+// @version      0.6
 // @description  hello world
 // @match        *://*/*
 // @grant        unsafeWindow
